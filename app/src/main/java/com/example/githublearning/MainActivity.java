9041034c448189c962bed66e379c875a9f9921b2
Shitaranjan <60256684+Shitaranjan@users.noreply.github.com>
2020-05-18 23:38:48 +0530

    Added new Button with Toast Message

--- app/src/main/java/com/example/githublearning/MainActivity.java
+++ app/src/main/java/com/example/githublearning/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.githublearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"HI github",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
